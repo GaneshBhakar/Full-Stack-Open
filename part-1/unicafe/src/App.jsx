3bf44267aec8209e9c56f875d0beaeb09d68c540
Ganesh Bhakar <mohitbhakar2004@gmail.com>
2026-02-03 19:05:04 +0530

    exercise 1.6 completed

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState} from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>{props.text}</button>
+  )
+}
+
+const Display = (props) => {
+  return (
+    <p>{props.text} {props.number}</p>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickGood = () =>{
+    setGood(good + 1)
+  }
+
+  const handleClickNeutral = () =>{
+    setNeutral(neutral + 1)
+  }
+
+  const handleClickBad = () =>{
+    setBad(bad + 1)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleClickGood} text='good' />
+      <Button onClick={handleClickNeutral} text='neutral' />
+      <Button onClick={handleClickBad} text='bad' />
+      <h2>Statistics</h2>
+      <Display text='good' number={good} />
+      <Display text='neutral' number={neutral} />
+      <Display text='bad' number={bad} />
+    </div>
   )
 }
 
