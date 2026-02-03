06f9eff79650e5aed12f49440333da0e275c20fa
Ganesh Bhakar <mohitbhakar2004@gmail.com>
2026-02-03 19:35:45 +0530

    Added states for total, average, positive

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -6,9 +6,9 @@ const Button = (props) => {
   )
 }
 
-const Display = (props) => {
+const Display = ({ text, number, suffix="" }) => {
   return (
-    <p>{props.text} {props.number}</p>
+    <p>{text} {number} {suffix}</p>
   )
 }
 
@@ -16,18 +16,27 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [total, setTotal] = useState(0)
 
   const handleClickGood = () =>{
     setGood(good + 1)
+    setAll(all + 1)
+    setTotal(total + 1)
   }
 
   const handleClickNeutral = () =>{
     setNeutral(neutral + 1)
+    setAll(all + 1)
+    setTotal(total + 0)
   }
 
   const handleClickBad = () =>{
     setBad(bad + 1)
+    setAll(all + 1)
+    setTotal(total - 1)
   }
+
 
   return (
     <div>
@@ -39,6 +48,9 @@ const App = () => {
       <Display text='good' number={good} />
       <Display text='neutral' number={neutral} />
       <Display text='bad' number={bad} />
+      <Display text='all' number={all} />
+      <Display text='average' number={total / all} />
+      <Display text='positive' number={(good / all) * 100} suffix="%" / >
     </div>
   )
 }
