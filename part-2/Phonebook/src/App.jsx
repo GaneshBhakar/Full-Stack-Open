5d5446fe9dbd0bffaf0f043acc41ced36c371fac
Ganesh Bhakar <mohitbhakar2004@gmail.com>
2026-02-23 22:15:45 +0530

    added the person and number as things to be displayed and controlled them

---
+++ part-2/Phonebook/src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hells',
+      number: 8328734634,
+    }
+  ])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const nameExists = persons.some(
+    person => person.name.toLowerCase() === newName.toLowerCase()
+  )
+
+  if(nameExists){
+    alert(`${newName} is already added to phoebook`)
+    return
+  }
+
+  //Handle form submit
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  //Handle input change
+  const handleNameChange = (event) => {
+    // console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)}/></div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <div>debug: {newName}</div>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <div key={person.name}>{person.name} {person.number}</div>
+      )}
+    </div>
+  )
+}
+
+export default App
