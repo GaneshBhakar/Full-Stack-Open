b4a70c0f8ab601e9208cdc89ab39ae2ccdecad04
Ganesh Bhakar <mohitbhakar2004@gmail.com>
2026-03-03 22:14:25 +0530

    Api fetrched, data filtered, components separated. only logic to display countries based on length is left

--- part-2/Countries/src/App.jsx
+++ part-2/Countries/src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect}  from 'react'
+import countryServices from './services/countries'
+import Filter from './components/filter'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [searched, setSearched] = useState('')
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    countryServices
+      .getAll()
+      .then(initialData => setData(initialData))
+  }, [])
+
+  const derived = data.filter(country =>
+    country.name.common.toLowerCase().includes(searched.toLowerCase())
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>find countries 
+      <Filter value={searched} onChange={(e) => setSearched(e.target.value)} />
+      <Countries countries={derived} />
+    </div>
   )
 }
 
