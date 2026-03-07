3c637b9a1e798cf1dfcded34da535ddff7e3bc12
Ganesh Bhakar <mohitbhakar2004@gmail.com>
2026-03-07 18:21:57 +0530

    validation added to phone number

--- part-3/models/persons.js
+++ part-3/models/persons.js
@@ -20,7 +20,17 @@ const personSchema = new mongoose.Schema({
 		minLength: [3, 'Name must be atleast 3 characters long'],
 		required: true
 	},
-	number: String
+	number: {
+		type: String,
+		required: true,
+		minLength: 8,
+		validate: {
+			validator: function(v) {
+				return /^\d{2,3}-\d+$/.test(v)
+			},
+			message: props => `${props.value} is not a valid phone number`
+		}
+	}
 })
 
 personSchema.set('toJSON', {
